c7db0212d9a7184d593e615f20cadf0105bc3222
dhayax <dhayanithi756@gmail.com>
2025-05-05 18:36:19 +0530

    added a basic signin routes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import { UserModel } from "./db";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 dotenv.config();
+
+const JWT_PASSWORD = process.env.JWT_SECRET as string;
 
 const dbHost = process.env.DB_Host as string;
 mongoose.connect(dbHost);
@@ -10,17 +14,46 @@ mongoose.connect(dbHost);
 const app = express();
 app.use(express.json());
 app.post("/api/v1/signup", async (req, res) => {
-  const {username, password } = req.body;
-  await UserModel.create({
+  const { username, password } = req.body;
+  
+  try {
+    await UserModel.create({
+      username,
+      password
+    });
+
+    res.json({
+      message: "user signed up ",
+    });
+  } catch (error) {
+    res.status(411).json({
+      message: "User already exist",
+    });
+  }
+});
+app.post("/api/v1/signin", async (req, res) => {
+  const { username, password } = req.body;
+  const response = await UserModel.findOne({
     username,
     password,
   });
 
-  res.json({
-    message: "user signed up ",
-  });
+  if (response) {
+    const token = jwt.sign(
+      {
+        id: response._id,
+      },
+      JWT_PASSWORD
+    );
+    res.json({
+      token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Incorrect credentials",
+    });
+  }
 });
-app.post("/api/v1/signin", (req, res) => {});
 app.post("/api/v1/content", (req, res) => {});
 app.get("/api/v1/content", (req, res) => {});
 app.delete("/api/v1/content", (req, res) => {});
@@ -29,6 +62,6 @@ app.post("/api/v1/brain/share", (req, res) => {});
 
 app.get("/api/v1/brain/:shareLink", (req, res) => {});
 
-app.listen(3000,()=>{
-  console.log(`PORT is Listening on 3000`)
-})
+app.listen(3000, () => {
+  console.log(`PORT is Listening on 3000`);
+});
