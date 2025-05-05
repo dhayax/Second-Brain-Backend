a2f53c2a4dd3f4373279ed66c16af5608a913988
dhayax <dhayanithi756@gmail.com>
2025-05-05 06:40:11 +0530

    added a basic signup route

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,34 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import { UserModel } from "./db";
+import mongoose from "mongoose";
+dotenv.config();
 
-const app=express()
+const dbHost = process.env.DB_Host as string;
+mongoose.connect(dbHost);
 
-app.post('/api/v1/signup',(req,res)=>{
+const app = express();
+app.use(express.json());
+app.post("/api/v1/signup", async (req, res) => {
+  const {username, password } = req.body;
+  await UserModel.create({
+    username,
+    password,
+  });
 
-})
-app.post('/api/v1/signin',(req,res)=>{
-    
-})
-app.post('/api/v1/content',(req,res)=>{
-    
-})
-app.get('/api/v1/content',(req,res)=>{
-    
-})
-app.delete('/api/v1/content',(req,res)=>{
-    
-})
+  res.json({
+    message: "user signed up ",
+  });
+});
+app.post("/api/v1/signin", (req, res) => {});
+app.post("/api/v1/content", (req, res) => {});
+app.get("/api/v1/content", (req, res) => {});
+app.delete("/api/v1/content", (req, res) => {});
 
-app.post('/api/v1/brain/share',(req,res)=>{
-    
-})
+app.post("/api/v1/brain/share", (req, res) => {});
 
-app.get('/api/v1/brain/:shareLink',(req,res)=>{
-    
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
+
+app.listen(3000,()=>{
+  console.log(`PORT is Listening on 3000`)
 })
